a301d76ee857c480d27a92c38b4667acc9229c1e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-05 10:03:21 -0600

    Make a method within Person-class public: can_use_services?

--- src/person.rb
+++ src/person.rb
@@ -10,13 +10,13 @@ class Person
 
   attr_accessor :age, :name
 
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
   private
 
   def of_age?
     @age >= 18
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
   end
 end
