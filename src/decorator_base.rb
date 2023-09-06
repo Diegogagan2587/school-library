12f5e1409ca2c5666bf47f4270c77c93d52e9a71
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-06 10:19:00 -0600

    Fix Logic inside TrimmerDecorator

--- src/decorator_base.rb
+++ src/decorator_base.rb
@@ -20,7 +20,7 @@ end
 
 class TrimmerDecorator < Decorator
   def correct_name
-    if @nameable.correct_name > 10
+    if @nameable.correct_name.length > 10
       @nameable.correct_name.slice(0,10)
     else
       @nameable.correct_name
