0faf9e7862ee0db5a4dd0049700a073e323f3420
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-05 22:12:40 -0600

    Add TrimmerDecorator class

--- src/decorator_base.rb
+++ src/decorator_base.rb
@@ -17,3 +17,13 @@ class CapitalizeDecorator < Decorator
     @nameable.correct_name.capitalize()
   end
 end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name > 10
+      @nameable.correct_name.slice(0,10)
+    else
+      @nameable.correct_name
+    end
+  end
+end
