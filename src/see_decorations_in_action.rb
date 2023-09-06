ea8b901ad1f5484cfc42e41cd8378c99efd9b955
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-06 10:20:12 -0600

    Add file: see_decorations_in_action_rb, so you can run it with ruby and test decorations

---
+++ src/see_decorations_in_action.rb
@@ -0,0 +1,11 @@
+require './person.rb'
+require './decorator_base.rb'
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
