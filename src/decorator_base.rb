59ef4e953b7a2e74d2b716c457db744fde7fced5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-06 16:05:01 -0600

    Removes additional decorator to implemt them on their own file

--- src/decorator_base.rb
+++ src/decorator_base.rb
@@ -13,18 +13,3 @@ class Decorator < Nameable
   end
 end
 
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    if @nameable.correct_name.length > 10
-      @nameable.correct_name.slice(0, 10)
-    else
-      @nameable.correct_name
-    end
-  end
-end
