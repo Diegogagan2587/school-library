42d7b6eb939fbf272bf7a19270db976597045560
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-07 15:11:29 -0600

    Implement association between classes: A book has many rentals

--- src/book.rb
+++ src/book.rb
@@ -1,8 +1,14 @@
 class Book
+  attr_reader :rentals
   attr_accessor :title, :author
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
   end
 end
