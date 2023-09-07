b03637a964f85c987768544e0fe7c973e0f1a55d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-07 15:41:34 -0600

    Initialize person and book in Rental class

--- src/rental.rb
+++ src/rental.rb
@@ -2,8 +2,12 @@ class Rental
     attr_accessor :date
     attr_reader :book, :person
 
-    def initialize(date)
+    def initialize(date, book, person)
         @date = date
+        @book = book
+        book.rentals << self
+        @person = person
+        person.rentals << self
     end
 
     def book = (book)
