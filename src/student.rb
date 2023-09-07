e99dbb1ddc43058e1a935b6fc1425893875e69b5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-07 15:54:45 -0600

    Fix linters errors

--- src/student.rb
+++ src/student.rb
@@ -12,7 +12,7 @@ class Student < Person
     '¯\\(ツ)/¯'
   end
 
-  def classroom = (classroom)
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
