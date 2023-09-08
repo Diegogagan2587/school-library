ca8c5691c3206e30e5ea051e4a3ae605d7c0e014
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-08 14:11:39 -0600

    Present user a list of options

---
+++ main.rb
@@ -0,0 +1,15 @@
+def main
+    puts "Welcome to School Library App!"
+
+    puts [
+        '1 - List all books',
+        '2 - List all people',
+        '3 - Create a Person',
+        '4 - Create a book',
+        '5 - Create a rental',
+        '6 - List all rentals for a given person id',
+        '7 - Exit'
+    ]
+end
+
+main()
