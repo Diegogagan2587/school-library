cd399e5f4a4030ddb7d475a44921d481226b0ee0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-08 18:55:18 -0600

    Fix Error: person was not accepting argument 'parent_permission'

--- src/person.rb
+++ src/person.rb
@@ -1,7 +1,7 @@
 require './nameable'
 
 class Person < Nameable
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     super()
     @id = Random.rand(1..1000)
     @name = name
